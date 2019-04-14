841207e758ff9233ff4cd8b8c99f56e09f303380
panyuhe <islandhe@gmail.com>
2019-04-14 16:00:04 -0500

    basic outline for fog nodes

--- fog/fognode.java
+++ fog/fognode.java
@@ -1,4 +1,4 @@
-package com.github.foghandoff.fog.fognode;
+package foghandoff.fog;
 
 import com.beust.jcommander.Parameter;
 
@@ -6,7 +6,33 @@ public class FogNode {
     private int port;
 
     private static class Predictor {
+        // TODO 
+    }
 
+    @Scope("prototype")
+    // Runnable class to listen for new connections
+    private static class ListenerRunnable implements Runnable{
+        public MyRunnable(){
+            
+        }
+
+        @Override
+        public void run(){
+
+        }
+    }
+
+    @Scope("prototype")
+    // Runnable class to communicate with clients after connection
+    private static class ServerRunnable implements Runnable{
+        public MyRunnable(Object parameter){
+        
+        }
+
+        @Override
+        public void run(){
+
+        }
     }
 
     public FogNode(int port){
