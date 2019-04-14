d5a8d1028fd368dc61efc6c10b66c1b5761b920a
panyuhe <islandhe@gmail.com>
2019-04-14 15:37:47 -0500

    nothing to see here

---
+++ fog/fognode.java
@@ -0,0 +1,15 @@
+package com.github.foghandoff.fog.fognode;
+
+import com.beust.jcommander.Parameter;
+
+public class FogNode {
+    private int port;
+
+    private static class Predictor {
+
+    }
+
+    public FogNode(int port){
+        this.port = port;
+    }
+}
