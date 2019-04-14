f4bbfe6e98bd1cec4ab3751987aac8ab0dccf56d
panyuhe <islandhe@gmail.com>
2019-04-14 17:05:08 -0500

    fix folder hierarchy to match tian's

--- src/main/java/foghandoff/fog/predictor.java
+++ src/main/java/foghandoff/fog/predictor.java
@@ -5,13 +5,7 @@ public class Predictor {
 }
 
 public class SmartPredictor extends Predictor {
-    private class Map {
-        public Map(){
-            
-        }
 
-        public 
-    }
 }
 
 public class CloudPredictor extends Predictor {
@@ -19,5 +13,5 @@ public class CloudPredictor extends Predictor {
 }
 
 public class DumbPredictor extends Predictor {
-
+    
 }
