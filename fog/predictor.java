f949807972d20cb095c679918e554280c09f4430
panyuhe <islandhe@gmail.com>
2019-04-14 16:52:08 -0500

    added new predictor types

---
+++ fog/predictor.java
@@ -0,0 +1,17 @@
+package foghandoff.fog;
+
+public class Predictor {
+    
+}
+
+public class SmartPredictor extends Predictor {
+
+}
+
+public class CloudPredictor extends Predictor {
+
+}
+
+public class DumbPredictor extends Predictor {
+    
+}
