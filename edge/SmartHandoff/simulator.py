076fbb8eff8dcd0c12dc472cf4c2405a60e0194d
hyunraider <hyunraider@yahoo.com>
2019-04-19 14:48:21 -0500

    edge changes

---
+++ edge/SmartHandoff/simulator.py
@@ -0,0 +1,11 @@
+import time
+
+interval = 300
+#Road east of UIUC Arboretum
+
+def send_data():
+    while True:
+        pass
+
+def simulator1():
+    fog_locations = [(40.092223, -88.211714), (40.093791, -88.211220), (40.094719, -88.212697)]
