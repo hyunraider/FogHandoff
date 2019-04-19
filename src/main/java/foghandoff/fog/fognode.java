1c63de16950898c3e529c079ef64a61ffb9b4f65
panyuhe <islandhe@gmail.com>
2019-04-18 20:11:37 -0500

    Weird naming bug

--- src/main/java/foghandoff/fog/fognode.java
+++ src/main/java/foghandoff/fog/fognode.java
@@ -1,63 +1,42 @@
 package foghandoff.fog;
 
 import java.net.*;
-import java.io.*;
+import java.io.*; 
 
 public class FogNode {
-    private ServerSocket serverSocket;
-    private Predictor predictor;
+    private int port;
+
+    private static class Predictor {
+        // TODO 
+    }
 
     @Scope("prototype")
     // Runnable class to listen for new connections
     private static class ListenerRunnable implements Runnable{
+        public MyRunnable(){
+            
+        }
+
         @Override
         public void run(){
-            while(true){
-                Socket clientSocket = serverSocket.accept();
 
-                ClientHandler handler = new ClientHandler(clientSocket);
-                Thread clientThread = new Thread(handler).start();
-            }
         }
     }
 
     @Scope("prototype")
     // Runnable class to communicate with clients after connection
-    private static class ClientHandler implements Runnable{
-        private Socket clientSocket;
-
-        public ClientHandler(Socket clientSocket){
-            this.clientSocket = clientSocket;
+    private static class ServerRunnable implements Runnable{
+        public MyRunnable(Object parameter){
+        
         }
 
         @Override
         public void run(){
-            PrintWriter out = new PrintWriter(clientSocket.getOutStream(), true);
-            Bufferedreader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            String inputLine;
-            while((inputLine = in.readLine()) != null){
-                // TODO 
-            }
-        }
-
-        @After
-        public void tearDown(){
-            clientSocket.stopConnection();
         }
     }
 
-    public FogNode(@Value("${serverPort}")int port, @Value("${predictorType}")String predictorType){
-        ServerSocket serverSocket = new ServerSocket(port);
-        if(predictorType == "cloud"){
-            predictor = new CloudPredictor();
-        } else if (predictorType == "smart"){
-            predictor = new SmartPredictor();
-        } else if (predictorType == "dumb"){
-            predictor = new DumbPredictor();
-        }
-
-        ListenerRunnable listener = new ListenerRunnable();
-        Thread t = new Thread(listener).start();
+    public FogNode(int port){
+        this.port = port;
     }
 }
--- src/main/java/foghandoff/fog/main.java
+++ src/main/java/foghandoff/fog/main.java
@@ -1,1 +1,7 @@
 package foghandoff.fog;
+
+@Slf4j
+public class Main {
+	
+	public static void main(String[] args) throws Exception {}
+}
